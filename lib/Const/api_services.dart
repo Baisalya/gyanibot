01b64f7e771734554b715fa9ce94e2eb812f09a9
Baisalya <baishalya1999@gmail.com>
2023-01-19 16:26:59 +0530

    Initial commit

---
+++ lib/Const/api_services.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+String api_key="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";
+class ApiServices{
+
+  String baseurl="https://api.openai.com/v1/completions";
+   Map<String,String>header={
+     "Content-Type":"application/json",
+     "Authorization":'Bearer $api_key',
+   };
+  sendMessage(String ?mesg)async{
+    var res=await http.post
+      (Uri.parse(baseurl),
+        headers:header,
+    body: jsonEncode({
+      "model":"text-davinci-003",
+      "prompt":"$mesg",
+      "temprature":0,
+      "max_token":100,
+      "top_p":1,
+      "frequency_penalty":0.0,
+      "presence_penalty":0.0,
+      "stop":["Human","AI"]
+
+    })
+    );
+    if(res.statusCode==200){
+      var data =jsonDecode(res.body.toString());
+      var msg=data["choices"][0]["text"];
+      return msg;
+    }else{
+      print("failed to fetch data");
+    }
+  }
+}
